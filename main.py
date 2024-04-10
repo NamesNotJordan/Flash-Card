b361aab90d4d47e5739a932e5abd51163a55d627
Jayden Hendrickse <Jayden.heberhendrickse@gmail.com>
2024-04-10 14:10:01 +0200

    Learned words are removed from the pool and tracked in a csv file

--- main.py
+++ main.py
@@ -1,51 +1,70 @@
 from tkinter import *
-from tkinter import messagebox
 import pandas
 import random
+import time
 
 # CONSTANTS
 BACKGROUND_COLOR = "#B1DDC6"
 TITLE_FONT = ("Ariel", 40, "italic")
 WORD_FONT = ("Ariel", 60, "bold")
+
+current_card = {}
 # --------LOGIC------------------
 
+try :
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    data = pandas.read_csv("data/french_words.csv")
 
-data = pandas.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
 
 
 def next_card():
+    global current_card, flip_timer
+    app.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_bg, image=CARD_FRONT_IMAGE)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    flip_timer = app.after(3000, flip_card)
+
 
 def flip_card():
-    pass
+    canvas.itemconfig(card_bg, image=CARD_BACK_IMAGE)
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+
+
+def word_has_been_learned():
+    to_learn.remove(current_card)
+    new_data = pandas.DataFrame(to_learn)
+    new_data.to_csv("data/words_to_learn.csv")
+    next_card()
 # ----------UI----------------------
+
 
 app = Tk()
 app.title("Flashy")
 app.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = app.after(3000, func=flip_card)
+
 # Card
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
-
-card_front_image = PhotoImage(file="images/card_front.png")
-card_back_image = PhotoImage(file="images/card_back.png")
-
-canvas.create_image(400, 263, image=card_front_image)
+CARD_FRONT_IMAGE = PhotoImage(file="images/card_front.png")
+CARD_BACK_IMAGE = PhotoImage(file="images/card_back.png")
+card_bg = canvas.create_image(400, 263, image=CARD_FRONT_IMAGE)
 card_title = canvas.create_text(400, 150, text="Title", font=TITLE_FONT)
 card_word = canvas.create_text(400, 263, text="Word", font=WORD_FONT)
 canvas.grid(row=0, column=0, columnspan=2)
 
 # Buttons
-right_image = PhotoImage(file="images/right.png")
-wrong_image = PhotoImage(file="images/wrong.png")
-
-right_button = Button(image=right_image, highlightthickness=0, command=next_card)
+RIGHT_IMAGE = PhotoImage(file="images/right.png")
+WRONG_IMAGE = PhotoImage(file="images/wrong.png")
+right_button = Button(image=RIGHT_IMAGE, highlightthickness=0, command=word_has_been_learned)
 right_button.grid(column=0, row=1)
 
-wrong_button = Button(image=wrong_image, highlightthickness=0, command=next_card)
+wrong_button = Button(image=WRONG_IMAGE, highlightthickness=0, command=next_card)
 wrong_button.grid(column=1, row=1)
 
 
